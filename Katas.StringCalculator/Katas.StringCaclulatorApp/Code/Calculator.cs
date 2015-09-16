d7331321c001127ab545354d6b51c8567915cbde
Joaquin Corchero <joaquin.corchero@gmail.com>
2015-09-16 14:22:17 +0100

    Implementation

--- Katas.StringCalculator/Katas.StringCaclulatorApp/Code/Calculator.cs
+++ Katas.StringCalculator/Katas.StringCaclulatorApp/Code/Calculator.cs
@@ -1,40 +1,123 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
+using System.Text;
 
 namespace Katas.StringCaclulatorApp.Code
 {
     public class Calculator
     {
-        private string _input;
-        private List<int> _numbers;
-
         public int Add(string input)
         {
-            _input = input;
+            if (string.IsNullOrEmpty(input))
+                return 0;
 
-            SetNumbersFromInput();
+            NumberCollector collector = new NumberCollector(input);
 
-            return Add();
+            return collector.Values.Sum();
         }
 
-        private void SetNumbersFromInput()
+    }
+
+    public class NumberCollector
+    {
+        private string _input;
+        internal List<int> Values { get; private set; }
+
+        public NumberCollector(string input)
         {
-            _numbers = new List<int> { 0 };
-            if (string.IsNullOrEmpty(_input))
+            _input = input;
+            PopulateValues();
+            var validator = new NegativeNumberValidator(Values);
+            validator.Validator();
+        }
+
+        private void PopulateValues()
+        {
+            Values = new List<int>();
+            var delimiters = new DelimiterExtractor(_input).GetDelimiter();
+
+            var items = _input.Split(delimiters)
+                .Where(i => !string.IsNullOrEmpty(i))
+                .ToList();
+
+            items.ForEach(i =>
+            {
+                var value = Convert.ToInt32(i);
+                if (value <= 1000)
+                    Values.Add(value);
+            });
+        }
+
+    }
+
+    public class NegativeNumberValidator
+    {
+        private List<int> _values = new List<int>();
+
+        public NegativeNumberValidator(List<int> values)
+        {
+            _values = values;
+        }
+
+        internal void Validator()
+        {
+            var negativeNumbers = _values.Where(n => n < 0).ToList();
+
+            if (!negativeNumbers.Any())
                 return;
 
-            var result = Regex.Split(_input, @"\D+");
-
-            result.ToList().ForEach(n =>
-                _numbers.Add(Convert.ToInt32(n))
-            );
+            RaiseNegativeNumberException(negativeNumbers);
         }
 
-        private int Add()
+        private static void RaiseNegativeNumberException(List<int> negativeNumbers)
         {
-            return _numbers.Sum();
+            StringBuilder builder = new StringBuilder();
+            builder.Append("negatives not allowed (");
+
+            negativeNumbers.ForEach(n =>
+            {
+                builder.AppendFormat(" {0}", n);
+            });
+
+            builder.Append(")");
+            throw new InvalidOperationException(builder.ToString());
+        }
+    }
+
+    public class DelimiterExtractor
+    {
+        private string _input;
+
+        public DelimiterExtractor(string fitsLine)
+        {
+            _input = fitsLine;
+        }
+
+        public char[] GetDelimiter()
+        {
+            List<char> result = new List<char> { '\r', '\n', ',', '/' };
+            if (!_input.StartsWith("//"))
+                return result.ToArray();
+
+            var numberOfMinusPosition = 2;
+            var lineChars = _input.ToCharArray();
+            for (int i = 1; i < lineChars.Length; i++)
+            {
+                numberOfMinusPosition = i;
+                if (lineChars[i] == '-' || Char.IsNumber(lineChars[i]))
+                    break;
+            }
+
+            var splitters = _input.Substring(2, numberOfMinusPosition - 2).ToCharArray().ToList();
+
+            splitters.ForEach(s =>
+            {
+                if (!result.Contains(s))
+                    result.Add(s);
+            });
+
+            return result.ToArray();
         }
     }
 }
--- Katas.StringCalculator/Katas.StringCalculator.Tests/Code/CalculatorTests.cs
+++ Katas.StringCalculator/Katas.StringCalculator.Tests/Code/CalculatorTests.cs
@@ -1,6 +1,7 @@
 ﻿using Katas.StringCaclulatorApp.Code;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NBehave.Spec.MSTest;
+using System;
 
 namespace Katas.StringCalculator.Tests.Code
 {
@@ -79,6 +80,112 @@ namespace Katas.StringCalculator.Tests.Code
         {
             _expected = 33;
             _input = "1\n2,30";
+
+            Execute();
+
+            _actual.ShouldEqual(_expected);
+        }
+
+        [TestMethod]
+        public void then_numbers_can_be_added_when_the_first_line_has_the_delimiter()
+        {
+            _expected = 3;
+            _input = "//;\n1;2";
+
+            Execute();
+
+            _actual.ShouldEqual(_expected);
+        }
+
+        [TestMethod]
+        public void then_a_string_with_negatives_throws_exception()
+        {
+            _input = "-20";
+            var expected = "negatives not allowed ( -20)";
+            var actual = string.Empty;
+            try
+            {
+                Execute();
+            }
+            catch (InvalidOperationException e)
+            {
+                actual = e.Message;
+            }
+            actual.ShouldContain(expected);
+        }
+
+        [TestMethod]
+        public void then_if_there_are_negative_numbers_they_are_displayed_on_the_exception()
+        {
+            _input = "-20,-3,4";
+            var expected = "negatives not allowed ( -20 -3)";
+            var actual = string.Empty;
+            try
+            {
+                Execute();
+            }
+            catch (InvalidOperationException e)
+            {
+                actual = e.Message;
+            }
+            actual.ShouldEqual(expected);
+        }
+
+        [TestMethod]
+        public void then_if_there_is_prefix_and_negative_numbers_they_are_displayed_on_the_exception()
+        {
+            _input = "//;-20;-3;4";
+            var expected = "negatives not allowed ( -20 -3)";
+            var actual = string.Empty;
+            try
+            {
+                Execute();
+            }
+            catch (InvalidOperationException e)
+            {
+                actual = e.Message;
+            }
+            actual.ShouldEqual(expected);
+        }
+
+        [TestMethod]
+        public void numbers_bigger_than_one_thousand_are_ignored()
+        {
+            _expected = 5;
+            _input = "1001,5";
+
+            Execute();
+
+            _actual.ShouldEqual(_expected);
+        }
+
+        [TestMethod]
+        public void then_the_delimiter_can_be_of_any_length()
+        {
+            _expected = 6;
+            _input = "//[***]\n1***2***3";
+
+            Execute();
+
+            _actual.ShouldEqual(_expected);
+        }
+
+        [TestMethod]
+        public void then_multiple_delimiters_are_allowed()
+        {
+            _expected = 6;
+            _input = "//[*][%]\n1*2%3";
+
+            Execute();
+
+            _actual.ShouldEqual(_expected);
+        }
+
+        [TestMethod]
+        public void then_multiple_delimiters_longer_than_one_char_are_allowed()
+        {
+            _expected = 6;
+            _input = "//[lkjl*][$$$%]\n1*2%3";
 
             Execute();
 
